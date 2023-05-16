8af2582ff017c674781ebd034906ebd99ccf6b7a
João Vidal <dev.joaovidal@gmail.com>
2023-05-16 11:27:08 -0300

    fatec-al02 first part done

---
+++ projects/fatec-al02/weather.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById('btn');
+addEventListener('keypress', function (e) {
+    if(e.key == 'Enter') {
+        btn.click();
+    }
+})
+
+async function getWeather() {
+    const city = document.getElementById('city').value;
+    const result = document.getElementById('resultado');
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d624370497d30ca21d54943f48656592&units=metric&lang=pt_br`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        result.textContent = `Temperatura atual em ${data.name}: ${data.main.temp}ºC`;
+    }).catch((error) => result.textContent = 'Cidade não encontrada')
+}
